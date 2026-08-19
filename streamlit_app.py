b8a6a10e8fb9d436909041389894e747e3b054f1
hassan khalid <hassankhalid@hassans-MacBook-Air.local>
2026-08-19 20:02:02 +0530

    Connect Streamlit to deployed API

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-API_URL = "http://127.0.0.1:8000/predict"
+API_URL = "https://cnn-chest-xray-pneumonia-detection.onrender.com/predict"
 
 st.set_page_config(
     page_title="Chest X-Ray Pneumonia Detection",
